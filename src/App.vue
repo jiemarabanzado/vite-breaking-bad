9f8ffe9500e5a760b95a78602be000d166b79ab3
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-30 09:25:40 +0100

    aggiunta la richiesta dei personaggi

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script >
-  
+  import SectionCharacters from './components/SectionCharacters.vue';
   export default{
     components:{
-      
+      SectionCharacters,
     }
   }
 </script>
 
 <template>
- 
+  <SectionCharacters/>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/SectionCharacters.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+export default{
+    data(){
+        return{
+            Characters:[],
+        }
+    },
+    created(){
+        axios.get('https://www.breakingbadapi.com/api/characters').then((resp)=>{
+            this.Characters=resp.data;
+            console.log(this.Characters);
+        });
+    }
+}
+</script>
+<template>
+
+</template>
+
+<style>
+</style>
