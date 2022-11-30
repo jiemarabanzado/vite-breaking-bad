791c291598e8a239516b6cfa1d910ef5baad1cb0
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-30 09:04:31 +0100

    creazione cartella

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script >
-  import PageTitle from './components/PageTitle.vue';
+  
   export default{
     components:{
-      PageTitle
+      
     }
   }
 </script>
 
 <template>
-  <PageTitle/>
+ 
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+   
 </style>
